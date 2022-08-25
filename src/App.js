1db09ece86d2c9ac3d5331bff9fe2ce8a1c2b5e4
Roshan Patel <roshanpatel722@gmail.com>
2022-08-26 00:30:35 +0530

    add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { makeStyles } from '@mui/material';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header'
+import CoinPage from './Pages/CoinPage';
+import Homepage from './Pages/Homepage';
 
 function App() {
+
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor:'#14161a',
+      color:'white',
+      minHeight: '100vh',
+    }
+  }));
+
+  const classes = useStyles()
+
   return (
-    <div className="App">
-      hello world
-    </div>
-  );
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Route path='/' component={Homepage} exact />
+        <Route path='/coins/:id' component={CoinPage} />
+      </div>
+    </BrowserRouter>
+    );
 }
 
 export default App;
